6fc775d78b0b89e4b76fd75e74913206db3e743f
ployt23 <revengeleagueof@gmail.com>
2025-09-08 14:28:39 +0530

    third

---
+++ src/exception.py
@@ -0,0 +1,33 @@
+import sys
+import logging
+from datetime import datetime
+import os
+
+# Setup basic logging
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+# Function to extract error details
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in Python script [{file_name}] at line [{line_number}]: {str(error)}"
+    return error_message
+
+# Custom exception class
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
